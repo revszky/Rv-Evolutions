406ccdc6705e999db53e7a6983d7678ee9f343e8
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-08-05 16:23:32 +0700

    progress

--- src/app/data/DataProduct.ts
+++ src/app/data/DataProduct.ts
@@ -1,17 +1,20 @@
-interface DataTag {
+interface DetailProduct {
   id: string;
   title: string;
+  price: string;
   description: string;
   image: string;
 }
 
-const DataResi: DataTag[] = [
+const DataProduct: DetailProduct[] = [
   {
     id: "0813",
     title: "Example",
+    price: "",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium eligendi animi dicta numquam, saepe laborum a consequatur cumque commodi ex.",
     image: "",
+    
   }
 ]
 
-export default DataResi;
+export default DataProduct;
