fd27224d99a94bccd6cc42f7d64235073873bf43
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-08-09 15:49:54 +0700

    halaman check id atau authenticity

--- src/app/(pages)/authenticity/page.tsx
+++ src/app/(pages)/authenticity/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchHorizontal from "@/app/components/search/SearchHorizontal";
+import Authenticity from "@/app/components/authenticity/Authenticity";
 
 export const metadata = {
   title: "RV • Authenticity",
@@ -8,9 +9,30 @@ export const metadata = {
 const page = () => {
   return (
     <main>
-      <section>
+      <section className="pt-20">
         <div>
-          <SearchHorizontal />
+          <Authenticity />
+        </div>
+      </section>
+
+      <section className="py-10">
+        <div className="flex flex-col items-center justify-center">
+          <div className="max-w-2xl">
+            <p className="font-mono text-sm text-center">
+              Enter the 9-digit RV code on the tag label into the column below
+              without using the hyphen "-". After you click "Search", the
+              results of our product authenticity verification will be displayed
+              below the column.
+            </p>
+          </div>
+
+          <div className="py-4">
+            <SearchHorizontal />
+          </div>
+
+          <div>
+            <p></p>
+          </div>
         </div>
       </section>
     </main>
---
+++ src/app/components/authenticity/Authenticity.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Authenticity = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-start justify-center gap-20">
+        <div>
+          <h1 className="font-mono">AUTHENTICITY</h1>
+        </div>
+
+        <div className="max-w-4xl">
+          <p className="font-mono text-sm">
+            We understand how important the authenticity and exclusivity of each
+            of our products is. Therefore, each of our clothing items comes with
+            a unique serial number ensuring that you are getting a genuine
+            product from RV. This serial number is not just identification, but
+            also a guarantee that you are getting the quality and design that we
+            have carefully designed.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Authenticity;
